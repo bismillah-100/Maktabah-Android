62d29fc9e34e117aaedc56de70524a8c4b23e4c6
bismillah-100 <bulletarmy@icloud.com>
2026-08-24 07:20:54 +0700

    chore: bump version to 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 1
-        versionName = "0.9"
+        versionName = "1.0"
 
         manifestPlaceholders["appName"] = "Maktabah"
 
