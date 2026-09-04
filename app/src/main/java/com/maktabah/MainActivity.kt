8dcd6a7d4ae32e23ed2f618c2be0e4a7dd54f326
bismillah-100 <bulletarmy@icloud.com>
2026-09-05 03:10:02 +0700

    feat: throttle annotation sync and upload widget snapshots

--- app/src/main/java/com/maktabah/MainActivity.kt
+++ app/src/main/java/com/maktabah/MainActivity.kt
@@ -193,18 +193,7 @@ class MainActivity : ComponentActivity() {
                                     is AnnotationChange.ReloadAll -> false
                                 }
                                 if (isLocal) {
-                                    kotlinx.coroutines.withContext(kotlinx.coroutines.Dispatchers.IO) {
-                                        val syncResult = cloudKitSyncManager.syncAnnotations(this@MainActivity, annotationManager)
-                                        if (syncResult != null && syncResult != "Success") {
-                                            kotlinx.coroutines.withContext(kotlinx.coroutines.Dispatchers.Main) {
-                                                android.widget.Toast.makeText(
-                                                    this@MainActivity,
-                                                    syncResult,
-                                                    android.widget.Toast.LENGTH_SHORT
-                                                ).show()
-                                            }
-                                        }
-                                    }
+                                    cloudKitSyncManager.scheduleAnnotationSync(this@MainActivity, annotationManager)
                                 }
                             }
                         }
