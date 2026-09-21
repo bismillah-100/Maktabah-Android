07d84181796b2c2bc8ae1c3050eb907d4ba99682
bismillah-100 <bulletarmy@icloud.com>
2026-09-21 13:24:15 +0700

    bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.maktabah.android"
         minSdk = 26
         targetSdk = 37
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
 
         manifestPlaceholders["appName"] = "Maktabah"
 
